4aa1e24948b84d21cb2578ce59f836eb1aef3298
maalireza51 <maalireza51@yahoo.com>
2022-04-14 23:59:49 +0430

    comment section mount and update

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -3,7 +3,7 @@ import Comment from './Comment'
 
 export default function Comments(props) {
     const comment = props.data.map(comment => {
-        return <Comment comment={comment.body}/>
+        return <Comment comment={comment.body}/> 
     })
     return (
         <div className="comments">
--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -29,7 +29,7 @@ export default class Content extends Component {
         if(prevprops.post.id !== this.props.post.id){
             this.setComment();
         }
-    }
+    } 
 
     render() {
         return (
